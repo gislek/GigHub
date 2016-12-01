a303e1373a29fdc51679ef09883c24b90a2ca2e0
gislek <gislekvalvik@gmail.com>
2016-12-01 13:32:49 +0100

    Refactor Cancel Gig

--- GigHub/Models/Notification.cs
+++ GigHub/Models/Notification.cs
@@ -6,12 +6,27 @@ namespace GigHub.Models
     public class Notification
     {
         public int Id { get; set; }
-        public DateTime DateTime { get; set; }
-        public NotificationType Type { get; set; }
+        public DateTime DateTime { get; private set; }
+        public NotificationType Type { get; private set; }
         public DateTime? OriginalDateTime { get; set; }
         public string OriginalVenue { get; set; }
 
         [Required]
-        public Gig Gig { get; set; }
+        public Gig Gig { get; private set; }
+
+        protected Notification()
+        {
+        }
+
+        public Notification(NotificationType type, Gig gig)
+        {
+            if (gig == null)
+                throw new ArgumentNullException("gig");
+
+            DateTime = DateTime.Now;
+            Type = type;
+            Gig = gig;
+
+        }
     }
 }
